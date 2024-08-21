85fb6dae21aa9edb9ffa663230e799562be91662
Rafał Tomczyk <rafal.tomczyk2000@gmail.com>
2024-08-21 19:06:51 +0200

    Update forms.py

--- table/forms.py
+++ table/forms.py
@@ -1,4 +1,8 @@
+from typing import Any, Mapping
 from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 from .models import Player, Match
 
 class createPlayer(forms.ModelForm):
@@ -6,3 +10,16 @@ class createPlayer(forms.ModelForm):
         model = Player
         fields = ['name']
         labels = {'name': 'Nazwa Gracza'}
+        
+class updateMatch(forms.ModelForm):
+    class Meta:
+        model = Match
+        fields = ['score1', 'score2', 'winner']
+        
+    def __init__(self, *args, **kwargs):
+        match = kwargs.get('instance')
+        super(updateMatch, self).__init__(*args, **kwargs)
+        if match:
+            self.fields['winner'].queryset = Player.objects.filter(id__in=[
+                match.player1.id, match.player2.id
+            ])
