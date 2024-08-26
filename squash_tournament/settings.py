72c7861784ee042eaa6ba7e279111b1c7b3cf1af
eric <rafal.tomczyk2000@gmail.com>
2024-08-26 17:53:44 +0200

    dziala

--- squash_tournament/settings.py
+++ squash_tournament/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fxie#-t8ah6ew2q^$qoed)x5e1wg!5vk_(r++z@p@x^d#x6*hc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
